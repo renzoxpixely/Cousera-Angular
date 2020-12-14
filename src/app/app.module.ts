b0171ba8cbda15cd82ad5b5d1f554e301b106436
renzoor <renzo.or@hotmail.com>
2020-12-14 11:27:23 -0500

    Practi 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,28 @@ import { MenuComponent } from './menu/menu.component';
 
 import { MatListModule } from '@angular/material/list';
 
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
     BrowserModule,
-    MatListModule
+    MatListModule,
+    
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
