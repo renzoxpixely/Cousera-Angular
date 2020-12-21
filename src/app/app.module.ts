d6ef5aaaf81442fb112df904597c08f32719b207
renzoor <renzo.or@hotmail.com>
2020-12-21 11:19:04 -0500

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,7 +40,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
